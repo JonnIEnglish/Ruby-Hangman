28e9919b7ef64da37b9be2a3538caddbd8bf49cc
Jonno <jonnovdriet@gmail.com>
2017-09-28 15:40:21 +0200

    V1 of project

---
+++ .py
@@ -0,0 +1,52 @@
+print ("Welcome!")
+
+import time
+time.sleep(1)
+
+name = input("Enter your name: )
+time.sleep(0.5)
+sur = input("Enter your surname: ")
+
+time.sleep(0.5)
+
+print ("Hello " + name + sur)
+
+time.sleep(0.5)
+
+Q1 = ("What do you like most about your job? ")
+Q2 = ("What do you like least about your job? ")
+Q3 = ("What tools do you use? ")
+Q4 = ("What languages do you use? ")
+Q5 = ("What is your favourite language? ")
+
+A1 = input(Q1)
+time.sleep(0.5)
+A2 = input(Q2)
+time.sleep(0.5)
+A3 = input(Q3)
+time.sleep(0.5)
+A4 = input(Q4)
+time.sleep(0.5)
+A5 = input(Q5)
+
+time.sleep(01)
+
+print("Thank you for completeing this survey.")
+
+time.sleep(1.5)
+
+print(Q1)
+print(A1)
+
+print(Q2)
+print(A2)
+
+print(Q3)
+print(A3)
+
+print(Q4)
+print(A4)
+
+print(Q5)
+print(A5)
+
---
+++ Survey.py
@@ -0,0 +1,76 @@
+print ("Welcome!")
+
+import time
+time.sleep(1)
+
+name = raw_input("Enter your name: ")
+time.sleep(0.5)
+sur = raw_input("Enter your surname: ")
+
+time.sleep(0.5)
+
+print ("Hello " + name + " " + sur)
+
+time.sleep(1.5)
+
+Q1 = ("What do you like most about your job? ")
+Q2 = ("What do you like least about your job? ")
+Q3 = ("What tools do you use? ")
+Q4 = ("What languages do you use? ")
+Q5 = ("What is your favourite language? ")
+
+A1 = raw_input(Q1)
+time.sleep(0.5)
+A2 = raw_input(Q2)
+time.sleep(0.5)
+A3 = raw_input(Q3)
+time.sleep(0.5)
+A4 = raw_input(Q4)
+time.sleep(0.5)
+A5 = raw_input(Q5)
+
+time.sleep(01)
+
+print("Thank you for completeing this survey.")
+
+time.sleep(1.5)
+
+print(Q1)
+time.sleep(1)
+print(A1)
+time.sleep(1)
+
+print ("")
+
+print(Q2)
+time.sleep(1)
+print(A2)
+time.sleep(1)
+
+print ("")
+
+print(Q3)
+time.sleep(1)
+print(A3)
+time.sleep(1)
+
+print ("")
+
+
+print(Q4)
+time.sleep(1)
+print(A4)
+time.sleep(1)
+
+print ("")
+
+
+print(Q5)
+time.sleep(1)
+print(A5)
+time.sleep(1)
+
+print ("")
+
+
+print ("done")
