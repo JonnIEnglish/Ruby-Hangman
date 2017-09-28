4b0d3ec3c43746afb46082331136aba5a13959b4
Jonno <jonnovdriet@gmail.com>
2017-09-28 16:33:54 +0200

    Initial Commit

---
+++ game.rb
@@ -0,0 +1,18 @@
+#print -> puts 
+#something = input() -> something = gets
+
+#if conditon
+#elsif
+#else
+#end
+
+#10.times do
+#end
+
+#[1,2,3].each do |item|
+
+#end
+
+#[1,2,3].each {|number| puts number}
+
+puts("Welcome to HANGMAN! ")
