1ea3737057f26efdcd559b2ed433745654568741
Jonno <jonnovdriet@gmail.com>
2017-10-02 23:18:09 +0200

    Added a 'main' instance and added necissary comments for easier reading.

--- Users.rb
+++ Users.rb
@@ -1,5 +1,5 @@
 class User
-    def initialize(name, age, gender, country)
+    def initialize(name, age, gender, country)  #Intializes all necisary variables.
         @name = name
         @age = age
         @gender = gender
@@ -7,16 +7,16 @@ class User
     end
 
     def info
-        puts "#{@name} is a #{@age} year old #{@gender} from #{@country}."
+        puts "#{@name} is a #{@age} year old #{@gender} from #{@country}." #Creates a simple infromation instance for easy reference.
     end
 end
 
-def create(user)
+def create(user) #Creates new users from user inputs.
     
     print "Enter your age: "
     age_1=gets.chomp;
 
-    print "Enter your gender male/female: "
+    print "Enter your gender male or female: "
     gender_1=gets.chomp;
 
     print "Enter your country of residence: "
@@ -24,26 +24,32 @@ def create(user)
 
     @user = user
 
-    @user=User.new(@user, age_1, gender_1, country_1)
+    @user=User.new(@user, age_1, gender_1, country_1) # @user is inputted by the user and therefore the instance takes their name.
 end
 
-while 1==1
-    
+def main # The main workings of the program.
+   
     puts 'Welcome! '
-    print 'Would you like to continue? Y or N: '
-    continue=gets.chomp;
-
-    if continue == "Y"
-        puts 'Great! Lets get started!'
-        
-        print "Enter your name: "
-        @user=gets.chomp;
-
-        create(@user)
-        
-        @user.info
-    else
-        break
     
+    while 1==1 # Creates an infinity loop.
+        
+        continue=gets.chomp;
+        print 'Would you like to continue? Y or N: '  # Askes if the user would like to proceed, if not it ends.
+        
+        if continue == "Y"
+            puts 'Great! Lets get started!'
+            
+            print "Enter your name: " # This is where the user creates the name for their particular instance.
+            @user=gets.chomp;
+        
+            create(@user) # Calls the 'create' instance which will guide the new user through the neccisary steps for creation.
+            
+            @user.info # Displays a nice sentance about the user.
+        else
+            break
+        
+        end
     end
 end
+
+main # Calls the 'main' instance and the creation begins!
